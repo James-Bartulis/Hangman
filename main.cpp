a9b47205eb73aa61a5dbb273543e84350530c04c
James Bartulis <jbart012@ucr.edu>
2019-12-27 23:48:54 -0800

    update fontsize

--- main.cpp
+++ main.cpp
@@ -7,10 +7,22 @@
 
 using namespace std;
 
+void SetFontSize(int size){
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 0;                   // Width of each character in the font
+	cfi.dwFontSize.Y = size;                  // Height
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
+
 int main(){
 
 	srand(time(NULL));
-
+	SetFontSize(52);
 	Hangman *test = new Hangman();
 
 	while(!test->isOver()){
@@ -26,4 +38,5 @@ int main(){
 	else
 		cout << endl << "Congratulations!!\n";
 	system("PAUSE");
+	SetFontSize(16);
 }
